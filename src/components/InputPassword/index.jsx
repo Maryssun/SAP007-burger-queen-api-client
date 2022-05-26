3a2b865cf59e4fa296adb0b38ea6c1aa696937e4
Maryssun <maryssol.dayanne@gmail.com>
2022-05-26 00:51:17 -0300

    feat: criação do componente InputPassword

---
+++ src/components/InputPassword/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Input } from "../Input";
+import style from "./inputPassword.style.module.css";
+
+export const InputPassword = (props) => {
+  const [eyeOpen, setEyeOpen] = useState(false);
+
+  function handleClickEye() {
+    setEyeOpen((oldEyeOpen) => !oldEyeOpen);
+  }
+
+  return (
+    <span className={style.container}>
+      <Input {...props} type={eyeOpen ? "text" : "password"} />
+      <span onClick={handleClickEye} className={style.spanEye}>
+        {eyeOpen ? <i class="bi bi-eye-fill"></i> : <i class="bi bi-eye-slash-fill"></i>}
+      </span>
+    </span>
+  );
+};
