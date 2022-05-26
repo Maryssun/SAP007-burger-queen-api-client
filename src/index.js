1c070e1cff78e923ce26d57f4f2ff5e8a4581cb1
Maryssun <maryssol.dayanne@gmail.com>
2022-05-26 00:48:49 -0300

    feat: instalação bootstrap-icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./global.style.module.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import { Router } from "./routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
