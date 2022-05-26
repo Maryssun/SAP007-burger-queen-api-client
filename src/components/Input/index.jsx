17f1b290c22d0a13de71bec845b4abd7cd1f5430
Maryssun <maryssol.dayanne@gmail.com>
2022-05-26 01:19:59 -0300

    refact: alteração do componente Input para receber mensagem de erro

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,15 +1,17 @@
 import style from "./input.style.module.css";
 
 export function Input(props) {
-
-    return (
-        <>
-            {props.label && <label>{props.label}</label>}
-            <input
-                className={style.input}
-                placeholder={props.placeholder}
-                type={props.type || "text"}
-                onInput={props.onInput} />
-        </>
-    );
+  return (
+    <>
+      {props.label && <label>{props.label}</label>}
+      <input
+        className={style.input}
+        placeholder={props.placeholder}
+        type={props.type || "text"}
+        onInput={props.onInput}
+        required={props.required}
+      />
+      {props.error && <div className={style.error}>{props.msgError}</div>}
+    </>
+  );
 }
