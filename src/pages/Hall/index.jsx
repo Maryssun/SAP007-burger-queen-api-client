df51b866c15a4dbaf76e71a11763f5462953d073
Maryssun <maryssol.dayanne@gmail.com>
2022-06-11 21:09:51 -0300

    feat: implementação de componente de produtos

--- src/pages/Hall/index.jsx
+++ src/pages/Hall/index.jsx
@@ -1,9 +1,28 @@
-// import style from "./hall.style.module.css";
+import { useEffect, useState } from "react";
+import { Product } from "../../components/Product";
+import { getProducts } from "../../services/products.service";
+import style from "./hall.style.module.css";
 
 export function Hall() {
+  const [products, setProduct] = useState([]);
+
+  useEffect(() => {
+    getProducts().then((produtos) => {
+      setProduct(produtos || []);
+    });
+  }, []);
+
   return (
     <>
-      <h1>Hall</h1>
+      <div className={style.hall}>
+        <h1>Hall</h1>
+
+        <div className={style.content}>
+          {products?.map((produto) => {
+            return <Product produto={produto} />;
+          })}
+        </div>
+      </div>
     </>
   );
 }
