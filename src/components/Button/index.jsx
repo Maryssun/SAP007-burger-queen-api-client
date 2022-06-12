ba1653d3b234584d7464800cf67adb505e08223e
Dayanne Maryssol <maryssol.dayanne@gmail.com>
2022-06-12 05:45:06 -0300

    feat: adição personalização no compoennte Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,22 @@
 import style from "./button.style.module.css";
 
 export function Button(props) {
+  let style_ = style.button;
+  
+  if(props.gold) {
+    style_ = style.buttonGold;
+  }
+  else if(props.white) {
+    style_ = style.buttonWhite;
+  }
+
   return (
     <>
       <button
-        className={props.gold ? style.buttonGold : style.button}
+        className={style_}
         id={props.id} 
         type={props.type || "button"}
+        disabled={props.disabled}
         onClick={props.onClick}>  
         {props.children}
       </button>
