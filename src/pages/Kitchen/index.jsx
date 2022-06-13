ea74b529ce3a42235cea787763464c056ff8691d
Dayanne Maryssol <maryssol.dayanne@gmail.com>
2022-06-12 21:17:31 -0300

    feat: criação da tela Kitchen

--- src/pages/Kitchen/index.jsx
+++ src/pages/Kitchen/index.jsx
@@ -1,9 +1,11 @@
-// import style from "./kitchen.style.module.css";
+import style from "./kitchen.style.module.css";
 
 export function Kitchen() {
+
   return (
     <>
-      <h1>Kitchen</h1>
+      <div className={style.kitchen}>
+      </div>
     </>
   );
 }
